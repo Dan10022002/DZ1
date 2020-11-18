932bd6f799b59f05905284fed6f407c405e6a1b1
Dan10022002 <70635329+Dan10022002@users.noreply.github.com>
2020-11-18 18:18:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,10 @@
 #include <vector>
 #include <cmath>
 #include <fstream>
+#include <bitset>
+#include <climits>
+
+#define BIT_NUMBER 8
 
 class Char_s
 {
@@ -10,7 +14,6 @@ class Char_s
         std::vector <char> Vec;
     public:
         char& operator[] (const char h);
-        char& operator<<= (const char h);
 };
 
 char& Char_s::operator[] (const char h)
@@ -18,46 +21,51 @@ char& Char_s::operator[] (const char h)
     return Vec[h];
 }
 
-char& Char_s::operator<<= (const char h)
-{
-    for (char h: Vec)
-    {
-        Vec[h]=Vec[h+1];
-    }
-}
-
-std::vector<char> Shifr (char n)
+std::vector<char> Key_generation (char n)
 {
     std::vector <char> Key;
-    for (int i=0; i<64; i++)
+    for (int i=0; i<n; i++)
     {
-        n=rand()%256;
-        n=abs(n);
-        Key.push_back(n);
+        int x=rand()%256;
+        x=abs(x);
+        Key.push_back(x);
     }
     return Key;
 }
 
-void Zamena (Char_s Vec, int kolvo_elementov, int g)
+void cyclicShiftLeft(std::vector<char> &vec,int n)
 {
-    Vec[kolvo_elementov]=g;
-}
-
-void Cikl (Char_s Vec, int kolvo_elementov)
-{
-    for (int i=0; i<4; i++)
-    {
-        int g=Vec[i];
-        Vec<<=1;
-        Zamena (Vec, kolvo_elementov, g);
+    size_t container=0;
+    for (int i=0;i<vec.size();i++){ // vec.size() == 8
+        container=(container|((long(vec[i]))<<(BIT_NUMBER*i)));
+    }
+    const int bits_in_long=BIT_NUMBER*sizeof(long);
+    long dest=(0|(container>>(bits_in_long-n)));
+    container<<=n;
+    container|=dest;
+    for (int i=0;i<vec.size();i++){
+        vec[i]=char(container>>(BIT_NUMBER*i));
     }
 }
 
-int main()
+void cyclicShiftRight(std::vector<char> &vec,int n)
 {
-    char n;
-    std::vector <char> Key = Shifr(n);
-    const int dlina = 64;
+    size_t container=0;
+    for (int i=0;i<vec.size();i++){ // vec.size() == 8
+        container=(container|((long(vec[i]))<<(BIT_NUMBER*i)));
+    }
+    const int bits_in_long=BIT_NUMBER*sizeof(long);
+    long dest=(0|(container<<(bits_in_long-n+1)));
+    container>>=n;
+    container|=dest;
+    for (int i=0;i<vec.size();i++){
+        vec[i]=char(container>>(BIT_NUMBER*i));
+    }
+}
+
+void Shifr()
+{
+    const int dlina = 8;
     std::ifstream infile("input.txt");
     std::vector <char> Vec;
     char ch;
@@ -68,7 +76,6 @@ int main()
     }
     for (int x: Vec)
     {
-        std::cout << x << " ";
         kolvo_simvolov+=1;
     }
     infile.close();
@@ -77,20 +84,81 @@ int main()
     {
         kolvo_blockov+=1;
     }
-    int s=kolvo_simvolov-(kolvo_simvolov%dlina);
+    int s=kolvo_simvolov;
     Char_s Block;
-    for (int i=0; i<s; i++)
+    std::ofstream outfile("output.txt");
+    for (char i=0; i<s; i++)
     {
-        if (i%dlina==63)
+        if ((i%dlina==7) || (i==(s-1)))
         {
-            Block.push_back(Vec[i]);
+            Block[i%dlina] = Vec[i];
+            std::vector <char> Key = Key_generation(dlina);
             for (int h=0; h<dlina; h++)
             {
-                Block[h]^=Key[h];
+                Key[h]^=Block[h];
             }
-            //Block
+            cyclicShiftLeft(Key,5);
+            for (char h: Key)
+            {
+                outfile << h;
+            }
         }
-        else Block.push_back(Vec[i]);
+        else Block[i%dlina] = Vec[i];
     }
+    outfile.close();
+}
+
+void Deshifr()
+{
+    const int dlina = 8;
+    std::ifstream infile("input.txt");
+    std::vector <char> Vec;
+    char ch;
+    int kolvo_simvolov=0;
+    while (infile.get(ch))
+    {
+        Vec.push_back(ch);
+    }
+    for (int x: Vec)
+    {
+        kolvo_simvolov+=1;
+    }
+    infile.close();
+    int kolvo_blockov=kolvo_simvolov/dlina;
+    if (kolvo_simvolov%dlina!=0)
+    {
+        kolvo_blockov+=1;
+    }
+    int s=kolvo_simvolov;
+    Char_s Block;
+    std::ofstream outfile("output.txt");
+    for (char i=0; i<s; i++)
+    {
+        if ((i%dlina==7) || (i==(s-1)))
+        {
+            Block[i%dlina] = Vec[i];
+            std::vector <char> Key = Key_generation(dlina);
+            for (int h=0; h<dlina; h++)
+            {
+                Key[h]^=Block[h];
+            }
+            cyclicShiftRight(Key,5);
+            for (char h: Key)
+            {
+                outfile << h;
+            }
+        }
+        else Block[i%dlina] = Vec[i];
+    }
+    outfile.close();
+}
+
+int main(int argc, char *argv[])
+{
+    bool t;
+    std::cout << "Code cryption is 0, code decryption is 1.\n";
+    std::cin >> t;
+    if (t==0) Shifr();
+    else Deshifr();
     return 0;
 }
